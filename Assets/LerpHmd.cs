3edbe94f15ec3d32e3eaa2c057ade310aace1b96
berendjan10 <berendjan10@live.nl>
2023-12-18 15:26:21 +0100

    improved script videos gemaakt voor meting

--- Assets/LerpHmd.cs
+++ Assets/LerpHmd.cs
@@ -13,6 +13,7 @@ public class LerpHmd : MonoBehaviour
     private Vector3 _goalPosition;
     private Quaternion _goalRotation;
     private bool _isPlaying = true;
+    public bool switchOnOff = false;
 
     // Start is called before the first frame update
     void Start()
@@ -24,35 +25,46 @@ public class LerpHmd : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        currentTime += Time.deltaTime; // add the time since last frame to the total duration
-
-        if(currentTime <= 2 || currentTime > 5) // no deviation
+        if (!switchOnOff) 
         {
-            _isPlaying = false;
-            transform.position = hmdTarget.position; // make sure that if simulation is not playing that avatar follows HMD.
+            transform.position = hmdTarget.position; // make sure that if simulation is not playing that avatar follows HMD????????
             transform.rotation = hmdTarget.rotation; // make sure that if simulation is not playing that avatar follows HMD.
         }
-        else if(currentTime > 2 && currentTime <= 3) // moving towards goal
+        else if (switchOnOff)
         {
-            _isPlaying = true;
-        }
-        else if(currentTime > 3 && currentTime <= 4) // at goal
-        {
-            _isPlaying = false;
-            transform.position = _goalPosition; 
-            transform.rotation = _goalRotation; 
-        }
-        else if(currentTime > 4 && currentTime <= 5) // moving back towards HMD
-        {
-            _isPlaying = true;
-        }
-        
+            currentTime += Time.deltaTime; // add the time since last frame to the total duration
 
-        if (_isPlaying) // je wil in 2 seconden 1 hele sinus doorlopen
-        {
-            transform.position = Vector3.Lerp(_goalPosition, hmdTarget.position, lerpValue); // position linear interpolation between HMD & target
-            transform.rotation = Quaternion.Slerp(transform.rotation, hmdTarget.rotation, lerpValue); // rotation linear interpolation between HMD & target
-            lerpValue = Mathf.Sin(2 * Mathf.PI * (currentTime - 2) / 2); // calc lerpValue used in 2 lines above. a sinus of the time, one full cycle in 2s.        }
+            if (currentTime <= 2 || currentTime > 4) // no deviation
+            {
+                _isPlaying = false;
+                transform.position = hmdTarget.position; // make sure that if simulation is not playing that avatar follows HMD.
+                transform.rotation = hmdTarget.rotation; // make sure that if simulation is not playing that avatar follows HMD.
+            }
+            else if (currentTime > 2 && currentTime <= 4) // moving towards goal
+            {
+                _isPlaying = true;
+            }
+            //else if (currentTime > 3 && currentTime <= 5) // at goal
+            //{
+            //    _isPlaying = false;
+            //    transform.position = _goalPosition;
+            //    transform.rotation = _goalRotation;
+            //}
+            //else if (currentTime > 5 && currentTime <= 6) // moving back towards HMD
+            //{
+            //    _isPlaying = true;
+            //}
+
+
+            if (_isPlaying) // je wil in 2 seconden 1 hele sinus doorlopen
+            {
+                transform.position = Vector3.Lerp(_goalPosition, hmdTarget.position, lerpValue); // position linear interpolation between HMD & target
+                transform.rotation = Quaternion.Slerp(transform.rotation, hmdTarget.rotation, lerpValue); // rotation linear interpolation between HMD & target
+                float amplitude = 0.5f; // Set the amplitude to 0.5 to make the cosine function go from 1 to 0 to 1
+                lerpValue = 0.5f + amplitude * Mathf.Cos(Mathf.PI * (currentTime - 2)); // Update the lerpValue calculation with the new amplitude
+                lerpValue = Mathf.Cos(Mathf.PI * (currentTime - 2)); // calc lerpValue used in 2 lines above. a sinus of the time, one full cycle in 2s.        }
+            }
         }
     }
+
 }
