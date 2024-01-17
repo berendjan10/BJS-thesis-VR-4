d6434d42062eca26a633b216d81b5919f587c19d
berendjan10 <berendjan10@live.nl>
2024-01-17 13:46:31 +0100

    Made some comments in script

--- Assets/LerpHmd.cs
+++ Assets/LerpHmd.cs
@@ -18,7 +18,7 @@ public class LerpHmd : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        _goalPosition = transform.position; // reference to object script is attached to
+        _goalPosition = transform.position; // reference to object script is attached to. Save the target position
         _goalRotation = transform.rotation; // reference to object script is attached to
     }
 
@@ -27,7 +27,7 @@ public class LerpHmd : MonoBehaviour
     {
         if (!switchOnOff) 
         {
-            transform.position = hmdTarget.position; // make sure that if simulation is not playing that avatar follows HMD????????
+            transform.position = hmdTarget.position; // make sure that if simulation is not playing that avatar follows HMD. SELF = HMD
             transform.rotation = hmdTarget.rotation; // make sure that if simulation is not playing that avatar follows HMD.
         }
         else if (switchOnOff)
@@ -37,7 +37,7 @@ public class LerpHmd : MonoBehaviour
             if (currentTime <= 2 || currentTime > 4) // no deviation
             {
                 _isPlaying = false;
-                transform.position = hmdTarget.position; // make sure that if simulation is not playing that avatar follows HMD.
+                transform.position = hmdTarget.position; // make sure that if simulation is not playing that avatar follows HMD. SELF = HMD
                 transform.rotation = hmdTarget.rotation; // make sure that if simulation is not playing that avatar follows HMD.
             }
             else if (currentTime > 2 && currentTime <= 4) // moving towards goal
@@ -61,8 +61,8 @@ public class LerpHmd : MonoBehaviour
                 transform.position = Vector3.Lerp(_goalPosition, hmdTarget.position, lerpValue); // position linear interpolation between HMD & target
                 transform.rotation = Quaternion.Slerp(transform.rotation, hmdTarget.rotation, lerpValue); // rotation linear interpolation between HMD & target
                 float amplitude = 0.5f; // Set the amplitude to 0.5 to make the cosine function go from 1 to 0 to 1
-                lerpValue = 0.5f + amplitude * Mathf.Cos(Mathf.PI * (currentTime - 2)); // Update the lerpValue calculation with the new amplitude
-                lerpValue = Mathf.Cos(Mathf.PI * (currentTime - 2)); // calc lerpValue used in 2 lines above. a sinus of the time, one full cycle in 2s.        }
+                lerpValue = 0.5f + amplitude * Mathf.Cos(Mathf.PI * (currentTime - 2)); // Update the lerpValue calculation with the new amplitude. FROM 1 TO 0 TO 1!
+                lerpValue = Mathf.Cos(Mathf.PI * (currentTime - 2)); // calc lerpValue used in 2 lines above. a sinus of the time, one full cycle in 2s.
             }
         }
     }
