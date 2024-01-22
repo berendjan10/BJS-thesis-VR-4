d6b4ca05fb9126763a3400b4a3add14d1bc68e71
berendjan10 <berendjan10@live.nl>
2024-01-22 16:57:42 +0100

    finished game. now on to 3PP creation. And I need to document

--- Assets/!!Made-by-BJS/Scripts/gameManagerExp1.cs
+++ Assets/!!Made-by-BJS/Scripts/gameManagerExp1.cs
@@ -1,35 +1,44 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-// hyperparameter: wait time TODO: change waittime back to 2-5s
-// hyperparameter: amount of instructions in phase 1 (no deviation)
-// hyperparameter: amount of instructions in phase 2 (with deviation)
-// hyperparameter: deviate_percentage: percentage that deviates in phase 2
-
+using Random = UnityEngine.Random;
 
 public class GameManagerExp1 : MonoBehaviour
 {
+
+    // ========== Hyperparameters: ==========
+
+    // wait time to sit straight between instructions
+    private float wait_time_lower_limit = 1.0f;
+    private float wait_time_upper_limit = 2.0f; // TODO: change waittime back to 2-5s
+
+    // amount of instructions in phase 1 (no deviation)
+    private int phase1_instructions = 1;
+
+    // amount of instructions in phase 2 (with deviation)
+    private int phase2_instructions = 6; // [2, 4, 6] uit [2, 3, 4, 5, 6, 7]
+
+    // percentage that deviates in phase 2
+    private int deviate_percentage = 33; // [%]
+
+    // ========== Variables: ==========
+
     public GameObject rightSphere;
     public GameObject leftSphere;
     public GameObject frontSphere;
     public GameObject backSphere;
     public GameObject topSphere;
-
     private List<GameObject> spheres; // List to store all the sphere game objects
-
     private ChangeText textScript; // Reference to the ChangeText script
-
     private float currentTime = 0f;
     private float waitTime;
     private int instructionCounter = 0; // Counter to keep track of the number of instructions given
     private bool deviate = false;
-    private int deviate_percentage = 50; // percentage that deviates in phase 2
-
+    private List<int> deviatingTrials = new List<int> { 2, 4, 6 }; // The trials in which avatar deviates from user
 
     void Start()
     {
-        //print("entered function: start");
         // Initialize the list with all sphere game objects
         spheres = new List<GameObject> { rightSphere, leftSphere, frontSphere, backSphere };
 
@@ -43,25 +52,21 @@ public class GameManagerExp1 : MonoBehaviour
             sphere.SetActive(false);
         }
 
-        // // Call the function to set up the initial game instructions
-        // SetGameInstructions();
-        SetRandomGameInstruction();
+        //// Calculate the number of trials that should deviate based on deviate_percentage
+        //int deviatingTrialsCount = Mathf.RoundToInt((float)(deviate_percentage * phase2_instructions) / 100);
 
+        //// Generate a collection of instruction counters for deviating trials
+        //GenerateDeviatingTrials(deviatingTrialsCount);
+
+        // Call the function to set up the initial game instructions
+        SetRandomGameInstruction();
     }
 
-    // void SetGameInstructions()
-    // {
-    //     // Set the initial game instruction
-    //     textScript.ChangeTextFcn("INITIAL Lean to the right - touch the golden sphere with your head");
-    // }
-
-    void OnTriggerEnter(Collider other)
+    void OnTriggerEnter(Collider other) // when the head touches a sphere
     {
-        //print("entered function: OnTriggerEnter");
         // Check if the collided object has the tag "GameTarget"
         if (other.gameObject.CompareTag("GameTarget"))
         {
-            //print("other gameobject == GameTarget");
             // Disable the sphere that was touched
             other.gameObject.SetActive(false);
 
@@ -70,14 +75,12 @@ public class GameManagerExp1 : MonoBehaviour
         }
         else if (other.gameObject.CompareTag("GameTargetTop"))
         {
-            //print("other gameobject == GameTargetTop");
             // Disable the sphere that was touched
             other.gameObject.SetActive(false);
             textScript.ChangeTextFcn("Good job! Please keep sitting straight until further instructions.");
 
-            // Set a random wait time between 1s and 5s
-            waitTime = Random.Range(1f, 2f);
-            print("waitTime: " + waitTime);
+            // Set a random wait time between 1s and 2s
+            waitTime = Random.Range(wait_time_lower_limit, wait_time_upper_limit);
 
             // Call the function to generate a new random game instruction after the wait period
             StartCoroutine(WaitAndSetRandomInstruction());
@@ -87,7 +90,6 @@ public class GameManagerExp1 : MonoBehaviour
     // Coroutine to wait for a random period and then set a new random game instruction
     IEnumerator WaitAndSetRandomInstruction()
     {
-        //print("entered function: WaitAndSetRandomInstruction (CoRoutine)");
         float elapsedTime = 0f;
 
         // Continue waiting until the elapsed time reaches the randomly chosen wait time
@@ -99,64 +101,111 @@ public class GameManagerExp1 : MonoBehaviour
             // Wait for the next frame
             yield return null;
         }
-        //print("timer done!");
 
         // Call the function to generate a new random game instruction
         SetRandomGameInstruction();
     }
 
+    // Coroutine (used when avatar deviates from user) to wait for a random period and then call the function to handle the logic after touching a sphere 
+    IEnumerator WaitAndHandleSphereTouched()
+    {
+        float elapsedTime = 0f;
+
+        // Continue waiting until the elapsed time reaches the randomly chosen wait time
+        while (elapsedTime < waitTime)
+        {
+            // Increment the elapsed time using Time.deltaTime
+            elapsedTime += Time.deltaTime;
+
+            // Wait for the next frame
+            yield return null;
+        }
+
+        // Call the function to handle the logic after touching a sphere
+        HandleSphereTouched();
+    }
+
     void HandleSphereTouched()
     {
-        //print("entered function: HandleSphereTouched");
-
-        // Increment the instruction counter
-        instructionCounter++;
-        print("Instruction Counter: " + instructionCounter);
-
-        textScript.ChangeTextFcn("Good job! Please sit straight up now.");
-        topSphere.SetActive(true);
+        if (instructionCounter > (phase1_instructions + phase2_instructions))
+        {
+            textScript.ChangeTextFcn("Thanks for playing!");
+        }
+        else
+        {
+            textScript.ChangeTextFcn("Good job! Please sit straight up now.");
+            topSphere.SetActive(true);
+        }
 
     }
 
     void SetRandomGameInstruction()
     {
-        if (instructionCounter <= 10) // Phase 1
+        // Increment the instruction counter
+        instructionCounter++;
+        print("instructionCounter: " + instructionCounter);
+
+        deviate = deviatingTrials.Contains(instructionCounter);
+        if (deviate)
         {
-            deviate = false;
+            LerpHmd lerpHmdInstance = GetComponent<LerpHmd>(); // Get a reference to the LerpHmd instance
+            lerpHmdInstance.TriggerAnimation(); // Call the TriggerAnimation() function
+            waitTime = lerpHmdInstance.duration;
+            StartCoroutine(WaitAndHandleSphereTouched());
+
+            // now. there is no golden sphere to activate a trigger.
         }
-        else if (instructionCounter > 10 && instructionCounter <= 20) // Phase 2
+        else
         {
-            deviate = true; // I want, in phase 2, for 'deviate_percentage'% of the trials to have the parameter 'deviate' set to true, but this should occur within a randomly selected collection of phase 2 trials.
+            // Generate a random index to choose the next sphere
+            int randomIndex = Random.Range(0, spheres.Count);
+
+            // Enable the selected sphere
+            spheres[randomIndex].SetActive(true);
+
+            // Set the game instruction based on the selected sphere
+            switch (randomIndex)
+            {
+                case 0:
+                    textScript.ChangeTextFcn("Please lean to the right - touch the golden sphere with your head");
+                    break;
+                case 1:
+                    textScript.ChangeTextFcn("Please lean to the left - touch the golden sphere with your head");
+                    break;
+                case 2:
+                    textScript.ChangeTextFcn("Please lean forward - touch the golden sphere with your head");
+                    break;
+                case 3:
+                    textScript.ChangeTextFcn("Please lean backward - touch the golden sphere with your head");
+                    break;
+                default:
+                    break;
+            }
+
         }
-        else if (instructionCounter > 20) // Phase 3
+        
+        
+        
+
+    }
+
+    // Function to generate a collection of instruction counters for deviating trials
+    void GenerateDeviatingTrials(int count)
+    {
+        deviatingTrials = new List<int>();
+
+        // Generate 'count' unique random instruction counters in the range [11, 20]
+        while (deviatingTrials.Count < count)
         {
-            deviate = false;
+            int trial = Random.Range((phase1_instructions + 1), (phase1_instructions + phase2_instructions + 1));
+            if (!deviatingTrials.Contains(trial))
+            {
+                deviatingTrials.Add(trial);
+            }
         }
-
-        // Generate a random index to choose the next sphere
-        int randomIndex = Random.Range(0, spheres.Count);
-
-        // Enable the selected sphere
-        spheres[randomIndex].SetActive(true);
-
-        // Set the game instruction based on the selected sphere
-        switch (randomIndex)
+        foreach (var item in deviatingTrials)
         {
-            case 0:
-                textScript.ChangeTextFcn("Please lean to the right - touch the golden sphere with your head");
-                break;
-            case 1:
-                textScript.ChangeTextFcn("Please lean to the left - touch the golden sphere with your head");
-                break;
-            case 2:
-                textScript.ChangeTextFcn("Please lean forward - touch the golden sphere with your head");
-                break;
-            case 3:
-                textScript.ChangeTextFcn("Please lean backward - touch the golden sphere with your head");
-                break;
-            default:
-                break;
-           }
-
+            print(item);
+        }
     }
 }
--- Assets/LerpHmd.cs
+++ Assets/LerpHmd.cs
@@ -2,8 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-// TODO: rechts maken.
-
 public class LerpHmd : MonoBehaviour
 {
     public Transform hmdTarget;
@@ -15,6 +13,7 @@ public class LerpHmd : MonoBehaviour
     private Quaternion _goalRotation;
     private bool _isPlaying = false;
     public bool deviate = false;
+    public float duration = 2.0f; // duration of deviation
 
 
     // Start is called before the first frame update
@@ -36,40 +35,37 @@ public class LerpHmd : MonoBehaviour
         {
             currentTime += Time.deltaTime; // add the time since last frame to the total duration
 
-            if (currentTime <= 2 || currentTime > 4) // no deviation
+            if (currentTime < 0 || currentTime > 2) // no deviation
             {
                 _isPlaying = false;
                 transform.position = hmdTarget.position; // make sure that if simulation is not playing that avatar follows HMD. SELF = HMD
                 transform.rotation = hmdTarget.rotation; // make sure that if simulation is not playing that avatar follows HMD.
             }
-            else if (currentTime > 2 && currentTime <= 4) // moving towards goal
+            else if (currentTime >= 0 && currentTime <= 2)  // Simulation
             {
                 _isPlaying = true;
             }
-            //else if (currentTime > 3 && currentTime <= 5) // at goal
-            //{
-            //    _isPlaying = false;
-            //    transform.position = _goalPosition;
-            //    transform.rotation = _goalRotation;
-            //}
-            //else if (currentTime > 5 && currentTime <= 6) // moving back towards HMD
-            //{
-            //    _isPlaying = true;
-            //}
-
 
             if (_isPlaying) // je wil in 2 seconden 1 hele sinus doorlopen
             {
                 float A = 0.5f; // amplitude
-                float period = 2 ; // period of the sine wave (how many seconds for 1 full cycle)
-                float B = Mathf.PI / Mathf.Abs(period); // frequency
-                float C = 2f; // phase shift of sine wave (horizontal shift)
-                float D = 0.5f; // vertical shift of the sine wave
-                lerpValue = A * Mathf.Sin(B * (currentTime - C)) + D; // Update the lerpValue calculation with the new amplitude. FROM 1 TO 0 TO 1! a sinus of the time, one full cycle in 2s.
+                float period = duration ; // period of the sine wave (how many seconds for 1 full cycle)
+                float B = 2*Mathf.PI / Mathf.Abs(period); // frequency
+                float C = duration/4; // phase shift of sine wave (horizontal shift)
+                float D = 0.5f; // vertical shift of the sine wave (0 to 1)
+                // Update the lerpValue calculation with the new amplitude
+                lerpValue = A * Mathf.Sin(B * (currentTime - C)) + D; // 0.5 * sin(pi * (x-0.5))+ 0.5 goes from 0 to 1 to 0 in 2s
                 transform.position = Vector3.Lerp(hmdTarget.position, _goalPosition,lerpValue); // position linear interpolation between HMD & target
                 // transform.rotation = Quaternion.Slerp(transform.rotation, hmdTarget.rotation, lerpValue); // rotation linear interpolation between HMD & target
             }
         }
+    }
+
+    public void TriggerAnimation()
+    {
+        currentTime = 0; // reset clock
+        deviate = true;
+        _isPlaying = true;
     }
 
 }
