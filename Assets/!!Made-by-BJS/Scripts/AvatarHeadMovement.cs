75e1bff6f4a13ddecb26510dc67d898f299e67b7
berendjan10 <berendjan10@live.nl>
2024-01-29 16:59:55 +0100

    currently working on reaching tasks with hands

--- Assets/!!Made-by-BJS/Scripts/AvatarHeadMovement.cs
+++ Assets/!!Made-by-BJS/Scripts/AvatarHeadMovement.cs
@@ -18,17 +18,15 @@ public class AvatarHeadMovement : MonoBehaviour
     public bool thirdPersonPerspective = false;
     public GameObject cameraOffset;
     public GameObject mirror;
-    private Vector3 standardCameraOffsetPosition = new Vector3(0.0f, 0.0f, 0.0f);
-    private Vector3 standardCameraOffsetRotation = new Vector3(0.0f, 0.0f, 0.0f);
-    public Vector3 thirdPersonPerspectiveOffsetPosition = new Vector3(0.0f, 0.0f, -0.5f);
-    public Vector3 thirdPersonPerspectiveOffsetRotation = new Vector3(0.0f, 0.0f, 0.0f);
+    private Vector3 standardCameraOffsetPosition = new Vector3();
+    private Vector3 standardCameraOffsetRotation = new Vector3();
+    public Vector3 thirdPersonPerspectiveOffsetPosition = new Vector3();
+    //public Vector3 thirdPersonPerspectiveOffsetRotation = new Vector3(15.0f, 0.0f, 0.0f);
 
-    //public GameObject leftHandTarget;
-    //private Vector3 standardLeftHandTargetPosition = new Vector3(-0.04640315845608711f, 1.0701793432235718f, -0.10970229655504227f);
-    //private Vector3 standardLeftHandTargetRotation = new Vector3(8.446f, 92.798f, 94.415f);
-    //public GameObject rightHandTarget;
-    //private Vector3 standardRightHandTargetPosition = new Vector3(0.0521f, -0.0642f, -0.0851f);
-    //private Vector3 standardRightHandTargetRotation = new Vector3(-9.95f, -104.846f, -85.265f);
+    public GameObject leftHandTarget;
+    public GameObject leftHandTargetFollow;
+    public GameObject rightHandTarget;
+    public GameObject rightHandTargetFollow;
 
     // Start is called before the first frame update
     void Start()
@@ -41,30 +39,31 @@ public class AvatarHeadMovement : MonoBehaviour
     void Update()
     {
         // Set camera offset
+        leftHandTargetFollow.transform.rotation = leftHandTarget.transform.rotation;
+        rightHandTargetFollow.transform.rotation = rightHandTarget.transform.rotation;
+
         if (!thirdPersonPerspective)
         {
             mirror.SetActive(true);
             cameraOffset.transform.position = standardCameraOffsetPosition;
-            cameraOffset.transform.rotation = Quaternion.Euler(standardCameraOffsetRotation); // 0,0,0
+            //cameraOffset.transform.rotation = Quaternion.Euler(standardCameraOffsetRotation); // 0,0,0
 
-            //leftHandTarget.transform.position = standardLeftHandTargetPosition;
-            //leftHandTarget.transform.rotation = Quaternion.Euler(standardLeftHandTargetRotation);
+            leftHandTargetFollow.transform.position = leftHandTarget.transform.position;
 
-            //rightHandTarget.transform.position = standardRightHandTargetPosition;
-            //rightHandTarget.transform.rotation = Quaternion.Euler(standardRightHandTargetRotation);
+            rightHandTargetFollow.transform.position = rightHandTarget.transform.position;
         }
         else if (thirdPersonPerspective)
         {
             mirror.SetActive(false);
 
             cameraOffset.transform.position = standardCameraOffsetPosition + thirdPersonPerspectiveOffsetPosition;
-            cameraOffset.transform.rotation = Quaternion.Euler(standardCameraOffsetRotation) * Quaternion.Euler(thirdPersonPerspectiveOffsetRotation);
+            //cameraOffset.transform.rotation = Quaternion.Euler(standardCameraOffsetRotation + thirdPersonPerspectiveOffsetRotation);
 
-            //leftHandTarget.transform.position -= thirdPersonPerspectiveOffsetPosition;
-            //leftHandTarget.transform.rotation = leftHandTarget.transform.rotation * Quaternion.Euler(-thirdPersonPerspectiveOffsetRotation);
+            leftHandTargetFollow.transform.position = leftHandTarget.transform.position - thirdPersonPerspectiveOffsetPosition;
+            //leftHandTargetFollow.transform.rotation = leftHandTarget.transform.rotation * Quaternion.Euler(-thirdPersonPerspectiveOffsetRotation);
 
-            //rightHandTarget.transform.position -= thirdPersonPerspectiveOffsetPosition;
-            //rightHandTarget.transform.rotation = rightHandTarget.transform.rotation * Quaternion.Euler(-thirdPersonPerspectiveOffsetRotation);
+            rightHandTargetFollow.transform.position = rightHandTarget.transform.position - thirdPersonPerspectiveOffsetPosition;
+            //rightHandTargetFollow.transform.rotation = rightHandTarget.transform.rotation * Quaternion.Euler(-thirdPersonPerspectiveOffsetRotation);
         }
 
         if (!deviate)
@@ -78,16 +77,22 @@ public class AvatarHeadMovement : MonoBehaviour
             {
                 // subtract offset so avatar stayus in place
                 transform.position = hmdTarget.position - thirdPersonPerspectiveOffsetPosition;
-                transform.rotation = hmdTarget.rotation * Quaternion.Euler(-thirdPersonPerspectiveOffsetRotation);
+                transform.rotation = hmdTarget.rotation; //* Quaternion.Euler(-thirdPersonPerspectiveOffsetRotation);
             }
         }
         else if (deviate)
         {
             currentTime += Time.deltaTime; // add the time since last frame to the total duration
 
-            if (currentTime < 0 || currentTime > 2) // no deviation
+            if (currentTime >= 0 && currentTime <= 2)  // Simulation
+            {
+                _isPlaying = true;
+            }
+
+            else if (currentTime < 0 || currentTime > 2) // no deviation
             {
                 _isPlaying = false;
+
                 // make sure that if simulation is not playing that avatar follows HMD. SELF = HMD
                 if (!thirdPersonPerspective)
                 {
@@ -98,12 +103,8 @@ public class AvatarHeadMovement : MonoBehaviour
                 {
                     // subtract offset so avatar stayus in place
                     transform.position = hmdTarget.position - thirdPersonPerspectiveOffsetPosition;
-                    transform.rotation = hmdTarget.rotation * Quaternion.Euler(-thirdPersonPerspectiveOffsetRotation);
+                    transform.rotation = hmdTarget.rotation; //* Quaternion.Euler(-thirdPersonPerspectiveOffsetRotation);
                 }
-            }
-            else if (currentTime >= 0 && currentTime <= 2)  // Simulation
-            {
-                _isPlaying = true;
             }
 
             if (_isPlaying) // je wil in 2 seconden 1 hele sinus doorlopen
